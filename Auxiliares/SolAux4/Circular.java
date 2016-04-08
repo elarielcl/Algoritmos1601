d50a360de033d9f4e03b0524632c3d6c1c4a256b
Ariel <el.ariel.cl@gmail.com>
2016-04-08 04:45:48 -0300

    Cambie el ciclo en n2 y subi condicion de quiebre al while

--- Auxiliares/SolAux4/Circular.java
+++ Auxiliares/SolAux4/Circular.java
@@ -4,7 +4,7 @@ public class Circular {
 	public static void main(String[] args) {
 		Nodo n1 = new Nodo(1,new Nodo(2, new Nodo(3,null)));
 		Nodo n2 = new Nodo(2,new Nodo(5,new Nodo(8,null)));
-		n2.sig.sig = n2.sig;
+		n2.sig.sig.sig = n2.sig;
 		System.out.println(esCircular(n1));
 		System.out.println(esCircular(n2));
 	}
@@ -17,15 +17,14 @@ public class Circular {
 		Nodo ref2 = first;
 		if(ref1==null) return false; //Caso de lista vacia
 		
-		while(ref1!=null && ref2.sig!=null){
+		while(ref1!=null && ref2 != null && ref2.sig!=null){
 			ref1 = ref1.sig;
 			ref2 = ref2.sig.sig; //ref2 avanza al doble de rapido que ref1
 			if(ref1==ref2)
 				return true;
-			if(ref2==null)
-				return false;
 		}
 		return false;
 	}
 	
 }
+
