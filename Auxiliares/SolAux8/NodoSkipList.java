4b9a458de001b5a5d29e24020e3317003caa77d3
Ariel <el.ariel.cl@gmail.com>
2016-05-12 19:19:13 -0300

    Solucion skip list aux 8

---
+++ Auxiliares/SolAux8/NodoSkipList.java
@@ -0,0 +1,17 @@
+public class NodoSkipList {
+	double info;
+	NodoSkipList[] sigs;
+	int n;
+	
+	static public boolean buscar(double x, NodoSkipList l) {
+		for (int i = l.n; i>=0; --i) {
+			if (l.sigs[i].info == x) return true;
+			else if (l.sigs[i].info < x) { //Si este nodo es menor que lo que busco, sigo buscando después de el
+				l = l.sigs[i];
+				i = l.n;
+			}
+		}
+		return l.info == x;
+	}
+}
+
